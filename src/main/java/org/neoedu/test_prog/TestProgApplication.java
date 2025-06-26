f842fee407eb513a218569369e28efc5f5c6dfa9
mrnkv <mrnkv@yahoo.com>
2025-06-26 12:05:30 +0300

    initial commit

---
+++ src/main/java/org/neoedu/test_prog/TestProgApplication.java
@@ -0,0 +1,13 @@
+package org.neoedu.test_prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProgApplication.class, args);
+	}
+
+}
