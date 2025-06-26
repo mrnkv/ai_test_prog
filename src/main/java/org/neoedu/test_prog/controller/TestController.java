acc06423adc80deb625e5c87b7b48fbeca4ba38a
mrnkv <mrnkv@yahoo.com>
2025-06-26 15:16:52 +0300

    web-starter added

---
+++ src/main/java/org/neoedu/test_prog/controller/TestController.java
@@ -0,0 +1,12 @@
+package org.neoedu.test_prog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Main Application!";
+    }
+}
